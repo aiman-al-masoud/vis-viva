67fee710ec7080229a95c07e4f0800e9da438b94
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-01 19:09:56 +0200

    ...

--- server/game.py
+++ server/game.py
@@ -10,6 +10,8 @@ class Game:
         self.__challenger = challenger
         self.__defender = defender
         self.__gameId = gameId
+        self.__challenger_event_queue = []
+        self.__defender_event_queue = []
         Game.GAME_POOL.append(self)
     
     def challenger(self)->str:
@@ -43,9 +45,25 @@ class Game:
     def __repr__(self):
         return f"Game(challenger={self.__challenger},defender={self.__defender})"
 
+    def add_event_for_challenger(self, event):
+        self.__challenger_event_queue.append(event)
+    
+    def add_event_for_defender(self, event):
+        self.__defender_event_queue.append(event)
+    
+    def get_events_for_challenger(self):
+        b = list(self.__challenger_event_queue)
+        self.__challenger_event_queue.clear()
+        return b
+
+    def get_events_for_defender(self):
+        b = list(self.__defender_event_queue)
+        self.__defender_event_queue.clear()
+        return b
+
     @staticmethod
     def games()->['Game']:
-        
+
         """
         Get all of the currently open Games.
         """
@@ -62,6 +80,7 @@ class Game:
         li = [g for g in Game.GAME_POOL if (g.challenger()==username) or (g.defender()==username)]
         return li[0] if len(li)!=0 else None
         
+
 
 
 
