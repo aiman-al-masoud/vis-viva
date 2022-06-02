e1887be505c64f4c5d667c9fbf8c691797655cf6
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-02 17:51:13 +0200

    ...

--- server/tests/simulate-game.sh
+++ server/tests/simulate-game.sh
@@ -20,6 +20,17 @@ sleep 0.1
 # other user receives fight-invite event 
 echo "fight-invite received by defender:"
 ./test-post.sh 'i-am-online' 'cammerlo' '{}'
+echo -e "\n"
+
+# other user accepts fight
+echo "other user accepts fight"
+./test-post.sh 'fight-accept' 'cammerlo' '{ "challenger" : "capra", "defender" : "cammerlo" , "gameId" : 1911 }'
+echo ""
+
+
+# challenger is ready:
+
+
 
 
 
