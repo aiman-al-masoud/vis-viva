f075443c380027c08e6ac9bad4a62aa1e7a107da
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-08 20:18:25 +0200

    ...

--- webapp/app/src/view/BattleUnitSprite.jsx
+++ webapp/app/src/view/BattleUnitSprite.jsx
@@ -26,7 +26,10 @@ export default class BattleUnitSprite extends Component {
                     <img src={this.props.battleUnit.getIcon()} className="battle-unit-sprite" />
                 </div>
 
-                <p>{this.props.battleUnit.health}</p>
+                {/* <p>{this.props.battleUnit.health}</p> */}
+                {/* extract health bar component and make it colored and w/ hp number */}
+                <progress id="health" value={this.props.battleUnit.health} max={this.props.battleUnit.maxHealth} ></progress>
+                {/* <input type="range" max={this.props.battleUnit.maxHealth} value={this.props.battleUnit.health} /> */}
             </div>)
     }
 }
