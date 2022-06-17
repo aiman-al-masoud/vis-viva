1ff4c4e1087b6cc9caed38a4f2e19808d226749d
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-17 18:38:50 +0200

    made battlefield grid keyboard operable

--- webapp/app/src/view/Square.jsx
+++ webapp/app/src/view/Square.jsx
@@ -24,9 +24,8 @@ export default class Square extends Component {
     render() {
         return (
 
-            <div onMouseEnter={this.select} onMouseLeave={this.unselect} onClick={this.props.onClick} className={this.state.className}>
+            <div onMouseEnter={this.select} onMouseLeave={this.unselect} onClick={this.props.onClick} className={this.state.className}  tabIndex="0"  onFocus={this.select} onBlur={this.unselect}>
                 {this.props.children}
-
             </div>
 
         )
@@ -35,13 +34,31 @@ export default class Square extends Component {
     select = () => {
         this.props.select(this.props.id)
         this.setState({ className: "square  square-highlighted" })
+
+        if(!this.keyListener){
+            window.addEventListener("keypress", this.keyListener = (e)=>{
+                if(e.code=="Enter"){
+                    console.log("clicked")
+                    this.props.onClick()
+                }
+            })
+        }
+        
     }
 
-    unselect = () => [
+    unselect = () => {
         this.setState({ className: "square" })
-    ]
+        window.removeEventListener("keypress",this.keyListener)
+        this.keyListener = undefined
+    }
 
+    // componentDidMount(){
+        
+    // }
 
+    componentWillUnmount(){
+        window.removeEventListener("keypress",this.keyListener)
+    }
 
 
 }
