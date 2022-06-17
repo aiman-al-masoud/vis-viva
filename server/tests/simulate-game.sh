9d080e436590cf8e3eb3ae7f61f5ad39d29baa68
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-17 18:58:38 +0200

    ...

--- server/tests/simulate-game.sh
+++ server/tests/simulate-game.sh
@@ -2,6 +2,12 @@
 
 # assume server is up and running 
 
+
+# shifts focus back on browser's search bar
+resetFocus(){
+    xdotool key Ctrl+l
+}
+
 # login a user on a clean window without cookies
 userLoginChrome(){
     username=$1
@@ -22,7 +28,7 @@ userLoginChrome(){
 
 
 
-pkill chromium
+pkill chrome
 xdotool key Ctrl+Alt+Down # switch workspace
 sleep 1
 userLoginChrome uno 
@@ -32,12 +38,45 @@ userLoginChrome due
 sleep 1
 xdotool key Ctrl+Alt+Up # back to workspace above
 sleep 1
+
 # uno challenges due
+
+# get to editable battle field
 xdotool key Tab
 xdotool key Return 
 sleep 0.5
 xdotool key Tab
 xdotool key Return 
+sleep 0.5
+
+# add some battle units
+xdotool key Tab
+xdotool key Tab
+xdotool key Return
+sleep 1
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Return 
+sleep 1
+
+# send challenge 
+resetFocus
+xdotool key Tab
+xdotool key Return 
+sleep 1
+
+# down to due
+xdotool key Ctrl+Alt+Down # switch workspace
+sleep 1
+# accept challenge
+resetFocus
+xdotool key Tab 
+xdotool key Return 
+
+
+
+
 
 
 
