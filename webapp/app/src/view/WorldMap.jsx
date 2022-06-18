e6d16133462c8be3ab7bbdfd771d7e6c96fdb266
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-18 18:28:59 +0200

    ...

--- webapp/app/src/view/WorldMap.jsx
+++ webapp/app/src/view/WorldMap.jsx
@@ -25,29 +25,34 @@ export default class WorldMap extends Component {
 
 
     render() {
-        // return (<div style={{backgroundImage:  `url(${BackgroundImage})` , height:"100vh", padding:"5px", backgroundPosition:"center", backgroundRepeat:"no-repeat" }}>
 
         return (<div>
 
-            <img src={BackgroundImage} width="200" />
+            <center>
 
-            <h1>World Map</h1>
 
-            <div>
-                <h2>Online Users</h2>
-                <p>Click on a user in the list to challenge them!</p>
+            <div style={{ display: "grid", gridTemplateColumns: "auto auto", width: "fit-content" }}  >
+               
+                <div>
+                    <h2>Online Users</h2>
+                    <p>Click on a user in the list to challenge them!</p>
 
-                <ul>
-                    {this.state.onlineUsers.map((u, i) => <li key={i}  >    <button onClick={() => { this.props.challengeUser(u) }}  > {u}</button>    </li>)}
-                </ul>
+                    <ul>
+                        {this.state.onlineUsers.map((u, i) => <li key={i}  >    <button onClick={() => { this.props.challengeUser(u) }}  > {u}</button>    </li>)}
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>Global Ranking</h2>
+                    <img src={BackgroundImage} width="200" />
+                    <ol >
+                        {this.state.usersXps.map((u, i) => <li key={i}    >         {u[0]}  - {u[1]}       </li>)}
+                    </ol>
+                </div>
             </div>
 
-            <div>
-                <h2>Global Ranking</h2>
-                <ol >
-                    {this.state.usersXps.map((u, i) => <li key={i}    >         {u[0]}  - {u[1]}       </li>)}
-                </ol>
-            </div>
+            </center>
+
 
         </div>)
     }
