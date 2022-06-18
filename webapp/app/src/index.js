b40595fae3aa7bc5c79b718f597a51e2934b3db3
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-18 17:13:45 +0200

    ...

--- webapp/app/src/index.js
+++ webapp/app/src/index.js
@@ -2,5 +2,14 @@ import React, {Component} from "react";
 import reactDom from "react-dom";
 import App from "./App.jsx";
 import "./index.css"
+import Favicon from "../res/icons/logo.png"
 
+
+//set favicon
+let link = document.createElement('link')
+link.rel = 'shortcut icon';
+link.href = Favicon;
+document.head.appendChild(link);
+
+//start app!
 reactDom.render(<App/>,  window.root)
