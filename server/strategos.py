5737780c908e1c9e87d65b5c7f7eeb70bbca0d86
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-18 02:17:14 +0200

    ...

--- server/strategos.py
+++ server/strategos.py
@@ -7,6 +7,7 @@ from events.classes.ready_event import *
 from events.classes.game_over_event import *
 from battle_unit import BattleUnit
 from game import Game
+from random import randint
 
 class Strategos:
 
@@ -26,22 +27,48 @@ class Strategos:
         from events.events import Events
 
         if isinstance(event, FightInviteEvent):
-
             e = FightAcceptEvent(event["challenger"], event["gameId"] , event["gameId"] )
             Events.instance().add_event(event["challenger"], e)
         
         elif isinstance(event, ReadyEvent):
-            
-            print("Strategos.add_event()", event)
+            battle_units = [BattleUnit(None, 2, "Samurai", 20, 20)]
+            self.game.set_battle_units( self.game.defender() , battle_units)
+            e = ReadyEvent(battle_units, event["gameId"])
+            e.set_sent_by_strategos(True)
+            Events.instance().add_event(self.game.challenger(), e)
+        
+        elif isinstance(event, FireEvent):
+            from_unit = event["fromUnit"]
+            to_unit = event["fromUnit"]
 
-            b = BattleUnit(None, 1, "Samurai", 20, 20)
-            e = ReadyEvent([b], event["gameId"])
+            print("Strategos.add_event()", from_unit, to_unit)
+
+            battle_units = self.game.get_battle_units(self.game.defender())
+            
+            for b in battle_units:
+                if b["position"]==to_unit["position"]:
+                    b["health"]-=from_unit["damage"]
+                    victim = b
+            
+            battle_units = [b for b in battle_units if b["health"] > 0]
+
+            self.game.set_battle_units(self.game.defender(), battle_units)
+
+            e = FireAckEvent(victim, event["id"], self.game.game_id(), victim["health"]<=0,  len(battle_units)==0 )
             e.set_sent_by_strategos(True)
             Events.instance().add_event(self.game.challenger(), e)
 
-            print("add_event()", "human player has fielded:", self.game.get_battle_units(self.game.challenger()))
+            # fire back
+            from_unit = battle_units[0]
+            to_unit = self.game.get_battle_units(self.game.challenger())[0]
+            e = FireEvent(from_unit, to_unit, randint(1, 999999999), self.game.game_id())
+            e.set_sent_by_strategos(True)
+            Events.instance().add_event(self.game.challenger(), e)
+            self.game.change_turn()
 
         
+        elif isinstance(event, FireAckEvent):
+            pass
 
 
         
