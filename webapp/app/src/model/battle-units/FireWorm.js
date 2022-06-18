bf4b695604a0b0d36ded66104d761c8d19f027d6
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-18 16:40:29 +0200

    ...

--- webapp/app/src/model/battle-units/FireWorm.js
+++ webapp/app/src/model/battle-units/FireWorm.js
@@ -3,6 +3,7 @@ import IdlingIcon from "../../../res/icons/fire-worm/idle.gif"
 import DyingIcon from "../../../res/icons/fire-worm/die.gif"
 import AttackingIcon from "../../../res/icons/fire-worm/attack.gif"
 import TakingHitIcon from "../../../res/icons/fire-worm/take-hit.gif"
+import AttackingSound from "../../../res/attack2.mp3"
 
 
 export default class FireWorm  extends BattleUnit{
@@ -19,6 +20,8 @@ export default class FireWorm  extends BattleUnit{
         this.__dying_icon = DyingIcon
         this.__attacking_icon = AttackingIcon
         this.__taking_hit_icon = TakingHitIcon
+        this.__attacking_sound = AttackingSound
+
     }
 
 
