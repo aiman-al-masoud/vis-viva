31ce94f85e4884afcafc558c7dd7e5569096a747
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-25 17:03:03 +0200

    cleanup

--- vis-viva/app/src/model/battle-units/classes/FireWorm.js
+++ vis-viva/app/src/model/battle-units/classes/FireWorm.js
@@ -1,11 +1,4 @@
 import BattleUnit from "../BattleUnit.js";
-// import IdlingIcon from "../../../../res/icons/fire-worm/idle.gif"
-// import DyingIcon from "../../../../res/icons/fire-worm/die.gif"
-// import AttackingIcon from "../../../../res/icons/fire-worm/attack.gif"
-// import TakingHitIcon from "../../../../res/icons/fire-worm/take-hit.gif"
-// import AttackingSound from "../../../../res/attack2.mp3"
-// import TakingHitSound from "../../../../res/damage2.mp3"
-
 const res = require.context('../../../../res/battle-units/fire-worm', false, /\.(png|jpe?g|svg|mp3|gif)$/)
 
 
@@ -21,12 +14,6 @@ export default class FireWorm  extends BattleUnit{
     constructor(){
         super(FireWorm.TYPE, 100, 15)
         this.importResources(res)
-        // this.__idling_icon = IdlingIcon
-        // this.__dying_icon = DyingIcon
-        // this.__attacking_icon = AttackingIcon
-        // this.__taking_hit_icon = TakingHitIcon
-        // this.__attacking_sound = AttackingSound
-        // this.__taking_hit_sound = TakingHitSound
         this.__attack_duration = 1600
         this.__die_duration = 1500
         this.__take_hit_duration = 600
--- vis-viva/app/src/model/battle-units/classes/Master.js
+++ vis-viva/app/src/model/battle-units/classes/Master.js
@@ -1,11 +1,4 @@
 import BattleUnit from "../BattleUnit.js";
-// import IdlingIcon from "../../../../res/icons/master/idle.gif"
-// import DyingIcon from "../../../../res/icons/master/die.gif"
-// import AttackingIcon from "../../../../res/icons/master/attack.gif"
-// import TakingHitIcon from "../../../../res/icons/master/take-hit.gif"
-// import AttackingSound from "../../../../res/attack.mp3"
-// import TakingHitSound from "../../../../res/damage1.mp3"
-
 const res = require.context('../../../../res/battle-units/master', false, /\.(png|jpe?g|svg|mp3|gif)$/)
 
 export default class Master extends BattleUnit {
@@ -19,12 +12,6 @@ export default class Master extends BattleUnit {
      */
     constructor() {
         super(Master.TYPE, 40, 20)
-        // this.__idling_icon = IdlingIcon
-        // this.__dying_icon = DyingIcon
-        // this.__attacking_icon = AttackingIcon
-        // this.__taking_hit_icon = TakingHitIcon
-        // this.__attacking_sound = AttackingSound
-        // this.__taking_hit_sound = TakingHitSound
         this.importResources(res)
         this.__attack_duration = 1200
         this.__die_duration = 1100
