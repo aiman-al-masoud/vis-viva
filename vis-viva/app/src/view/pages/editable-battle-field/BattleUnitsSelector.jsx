322428e88d624b4ff2668e2030d410bbb94d5c0f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-26 15:57:46 +0200

    repositioning stats table

--- vis-viva/app/src/view/pages/editable-battle-field/BattleUnitsSelector.jsx
+++ vis-viva/app/src/view/pages/editable-battle-field/BattleUnitsSelector.jsx
@@ -17,38 +17,42 @@ export default class BattleUnitsSelector extends Component {
      * getTypeStats : (string)=>UnitStats
      * }} props 
      */
-    constructor(props){
+    constructor(props) {
         super(props)
         this.props = props
     }
 
-    render(){
+    render() {
 
-        return (<div style={{padding:0, margin:0, width:"fit-content"}}>
+        return (<div style={{ padding: 0, margin: 0, width: "fit-content" }}>
 
             <center>
-            <h2>{this.props.selectedType}</h2>
-                <img src={this.props.getIcon(this.props.selectedType)} width="200"/>
-                <br />
-                {this.props.getTypeStats(this.props.selectedType).prettyPrintHtml()}
+                <h2>{this.props.selectedType}</h2>
 
-                <Button onClick={this.prev} title="Previous"  icon={ArrowIcon}  flippedX={true} />
-                <Button onClick={this.next} title="Next"  icon={ArrowIcon}  />
+
+                <div style={{ display: "grid", gridTemplateColumns: "auto 4vw" }} >
+                    <img src={this.props.getIcon(this.props.selectedType)} width="200" />
+                    {/* <br /> */}
+                    {this.props.getTypeStats(this.props.selectedType).prettyPrintHtml()}
+                </div>
+
+                <Button onClick={this.prev} title="Previous" icon={ArrowIcon} flippedX={true} />
+                <Button onClick={this.next} title="Next" icon={ArrowIcon} />
             </center>
-           
+
         </div>)
     }
 
 
-    next = ()=>{
+    next = () => {
         let currIndex = this.props.types.indexOf(this.props.selectedType)
-        return this.props.selectType(this.props.types[currIndex+1] ?? this.props.selectedType)
+        return this.props.selectType(this.props.types[currIndex + 1] ?? this.props.selectedType)
     }
 
-    prev = ()=>{
+    prev = () => {
         let currIndex = this.props.types.indexOf(this.props.selectedType)
-        return this.props.selectType(this.props.types[currIndex-1] ?? this.props.selectedType)
+        return this.props.selectType(this.props.types[currIndex - 1] ?? this.props.selectedType)
     }
 
-    
+
 }
