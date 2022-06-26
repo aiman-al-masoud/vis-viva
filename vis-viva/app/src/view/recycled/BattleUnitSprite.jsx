7abb22f70e41e3b0c5d0ddfd3ede8f257e23d41b
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-26 15:30:08 +0200

    flipped health bar for enemies

--- vis-viva/app/src/view/recycled/BattleUnitSprite.jsx
+++ vis-viva/app/src/view/recycled/BattleUnitSprite.jsx
@@ -36,7 +36,7 @@ export default class BattleUnitSprite extends Component {
 
             {/* <p>{this.props.battleUnit.health}</p> */}
             {/* extract health bar component and make it colored and w/ hp number */}
-            <progress id="health" value={this.props.battleUnit.health} max={this.props.battleUnit.maxHealth} ></progress>
+            <progress id="health" value={this.props.battleUnit.health} max={this.props.battleUnit.maxHealth} style={this.props.invertedForEnemy ? { transform: "scaleX(-1)" } : { transform: "scaleX(1)" }}></progress>
             {/* <input type="range" max={this.props.battleUnit.maxHealth} value={this.props.battleUnit.health} /> */}
         </div>)
     }
