1f9c6cac8e39193c319c415020a59043f7523be4
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-27 17:16:04 +0200

    removed state from Settings.jsx

--- vis-viva/app/src/view/pages/settings/Settings.jsx
+++ vis-viva/app/src/view/pages/settings/Settings.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-// import L from "../../../model/utils/Language";
 import L from "../../../model/utils/Language";
 import S from "../../../model/utils/Settings";
 
@@ -10,26 +9,16 @@ export default class Settings extends Component{
 
     constructor(props){
         super(props)
-
-        this.state = {
-            APP_LANGUAGE : S.getInstance().get(S.APP_LANGUAGE)
-        }
     }
 
     onSet = (key, event) => {
-
         let choice = event.target
         choice = choice.options[choice.selectedIndex].text
-        console.log(key, choice)
+        console.log("onSet()", key, choice)
         S.getInstance().set(key, choice)
-        let newState = {}
-        newState[key] = choice
-        this.setState(newState)
-
-        if(key==S.APP_LANGUAGE){
-            L.reload() 
-            this.props.update()
-        }
+        L.reload() 
+        this.props.update()
+        this.forceUpdate()
     }
 
 
@@ -39,7 +28,7 @@ export default class Settings extends Component{
             <h1>{L.settings}</h1>
 
             <h1>Language</h1>
-            <select value={this.state.APP_LANGUAGE} onChange={(event) => { this.onSet(S.APP_LANGUAGE, event)  }} >
+            <select value={S.getInstance().get(S.APP_LANGUAGE)} onChange={(event) => { this.onSet(S.APP_LANGUAGE, event)  }} >
                 {L.available().map((opt, index) => { return <option title={opt} key={index}>{opt}</option> })}
             </select>
 
