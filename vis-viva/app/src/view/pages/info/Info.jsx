22d44ef3e99a9dbbd252fe3e9086c53d3c7af0c6
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-28 12:27:01 +0200

    improving description

--- vis-viva/app/src/view/pages/info/Info.jsx
+++ vis-viva/app/src/view/pages/info/Info.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import LogoIcon from "../../../../res/icons/logo.png"
 
 /**
  */
@@ -10,6 +11,9 @@ export default class Info extends Component {
 
     render() {
         return (<div >
+
+            <img src={LogoIcon} width="50px" style={{float:"right"}}/>
+
             <h1>Info</h1>
 
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
@@ -22,15 +26,14 @@ export default class Info extends Component {
 
                     <h2>Gameplay</h2>
 
-                
-                    
+
 
                     <h2>Other Links</h2>
-                    <p>Please check out my other Projects:</p>
-                    <ul>
-                        <li ><a href="https://psittacus.eu.pythonanywhere.com" target="_blank">Psittacus</a></li>
-                    </ul>
+                    <span>Please check out  <a href="https://aiman-al-masoud.github.io/" target="_blank">my other projects</a>, and especially this one: </span>
+                    <a href="https://psittacus.eu.pythonanywhere.com" target="_blank">Psittacus</a>.
 
+                    <p>Liked the game? Do you want to see more similar games? Do you have any game ideas to share? Contact me at:</p>
+                    <a href="mailto:luxlunarislabs@gmail.com">luxlunarislabs@gmail.com</a>
 
                 </div>
             </div>
