6677de3bfeaf769aa5b36cd133626a59301388eb
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-06-28 18:14:14 +0200

    updated package.json

--- vis-viva/scripts/dev-build.sh
+++ vis-viva/scripts/dev-build.sh
@@ -2,6 +2,6 @@ webpack   --mode=development
 echo "<script>" >> ./dist/index.html  
 cat ./dist/main.js >> ./dist/index.html  
 echo "</script>" >> ./dist/index.html  
-sed -i 's/<script defer="defer" src="main.js">//g' ./dist/index.html   
+sed -i 's/<script defer src="main.js"></script>//g' ./dist/index.html   
 rm ./dist/main.js
 rm ./dist/main.js.LICENSE.txt
